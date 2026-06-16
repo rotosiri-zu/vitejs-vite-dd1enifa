2b818538380f00480c3cb6c05fa5cf11aa43ef56
rotosiri-zu <hongjitufang25@gmail.com>
2026-06-16 11:50:02 +0900

    ヘッダーの作成(ルーティング機能実装)

--- src/components/molecules/MenuDrawer.tsx
+++ src/components/molecules/MenuDrawer.tsx
@@ -4,18 +4,21 @@ import { Button, Drawer, DrawerOverlay, DrawerContent, DrawerBody } from "@chakr
 type Props = {
     onClose: () => void;
     isOpen: boolean;
+    onClickHome: () => void;
+    onClickUserManagement: () => void;
+    onClickSetting: () => void;
 }
 
 export const MenuDrawer: VFC = memo((props) => {
-    const { onClose, isOpen } = props;
+    const { onClose, isOpen, onClickHome, onClickUserManagement, onClickSetting } = props;
     return (
         <Drawer placement="left" size="xs" onClose={onClose} isOpen={isOpen}>
             <DrawerOverlay>
                 <DrawerContent>
                     <DrawerBody p={0} bg="gray.100">
-                        <Button w="100%">Top</Button>
-                        <Button w="100%">ユーザー一覧</Button>
-                        <Button w="100%">設定</Button>
+                        <Button w="100%" onClick={onClickHome}>Top</Button>
+                        <Button w="100%" onClick={onClickUserManagement}>ユーザー一覧</Button>
+                        <Button w="100%" onClick={onClickSetting}>設定</Button>
                     </DrawerBody>
                 </DrawerContent>
             </DrawerOverlay>
