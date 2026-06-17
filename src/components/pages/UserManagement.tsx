ce8df561fab9fce29da93e19f00c93cb6490f178
rotosiri-zu <hongjitufang25@gmail.com>
2026-06-17 11:11:08 +0900

    ユーザー一覧画面の作成

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,5 +1,14 @@
 import { memo, VFC } from "react";
+import { Wrap, WrapItem } from "@chakra-ui/react";
+
+import { UserCard } from "../organisms/user/UserCard";
 
 export const UserManagement: VFC = memo(() => {
-    return <p>ユーザー管理ページです</p>
+    return (
+        <Wrap p={{ base: 4, md: 10 }}>
+            <WrapItem>
+                <UserCard imageUrl="https://source.unsplash.com/random" userName="ひろき" fullName="Hiroki hijikata" />
+            </WrapItem>
+        </Wrap>
+    )
 });
