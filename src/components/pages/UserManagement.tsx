e52fcc33a34bb3f7db2618fd02520ee7fb4aa079
rotosiri-zu <hongjitufang25@gmail.com>
2026-06-17 13:35:18 +0900

    ユーザー一覧取得機能の実装

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,14 +1,33 @@
 import { memo, VFC } from "react";
-import { Wrap, WrapItem } from "@chakra-ui/react";
+import { Center, Spinner, Wrap, WrapItem } from "@chakra-ui/react";
 
 import { UserCard } from "../organisms/user/UserCard";
+import { useAllUsers } from "../../hooks/useAllUsers";
 
 export const UserManagement: VFC = memo(() => {
+    const { getUsers, users, loading } = useAllUsers();
+
+    useEffect(() => getUsers(), [])
+
     return (
-        <Wrap p={{ base: 4, md: 10 }}>
-            <WrapItem>
-                <UserCard imageUrl="https://source.unsplash.com/random" userName="ひろき" fullName="Hiroki hijikata" />
-            </WrapItem>
-        </Wrap>
+        <>
+            {loading ? (
+                <Center h="100vh">
+                    <Spinner />
+                </Center>
+            ) : (
+                <Wrap p={{ base: 4, md: 10 }}>
+                  {users.map((user) => (
+                      <WrapItem key={user.id} mx="auto">
+                        <UserCard 
+                        imageUrl="https://source.unsplash.com/random" 
+                        userName="ひろき" 
+                        fullName="Hiroki hijikata" 
+                        />
+                      </WrapItem>
+                  ))}
+                </Wrap>
+            )}
+        </>
     )
 });
